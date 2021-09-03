49a2e8f0f894f9791c02b80ea8fa8f861ba9a9d4
Steven Schreiber <53790515+StevenRS00@users.noreply.github.com>
2021-09-03 13:49:55 -0700

    Added Retrofit and am able to make calls to the API.
    I also added a view to see the response from the API.

---
+++ app/src/main/java/info/steven/wk01hw02/JsonPlaceHolderApi.java
@@ -0,0 +1,12 @@
+package info.steven.wk01hw02;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
